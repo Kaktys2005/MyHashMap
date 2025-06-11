afe8131a4f3d97e507fc398c42b34355cdeeb1c1
Artem <Kaktys2025@yandex.ru>
2025-06-11 16:14:13 +0300

    MyHashMap

---
+++ src/MyHashMap.java
@@ -0,0 +1,106 @@
+public class MyHashMap<K, V> {
+    private static final int DEFAULT_CAPACITY = 16;
+    private static final double LOAD_FACTOR = 0.75;
+
+    private MyLinkedList<Entry<K, V>>[] buckets;
+    private int size;
+
+    private static class Entry<K, V> {
+        K key;
+        V value;
+
+        Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    public MyHashMap() {
+        this(DEFAULT_CAPACITY);
+    }
+
+    public MyHashMap(int capacity) {
+        buckets = (MyLinkedList<Entry<K, V>>[]) new MyLinkedList[capacity];
+        for (int i = 0; i < capacity; i++) {
+            buckets[i] = new MyLinkedList<>();
+        }
+        size = 0;
+    }
+
+    public void put(K key, V value) {
+        if ((double) size / buckets.length >= LOAD_FACTOR) {
+            resize();
+        }
+
+        int index = getIndex(key);
+        MyLinkedList<Entry<K, V>> bucket = buckets[index];
+
+        for (Entry<K, V> entry : bucket) {
+            if (entry.key.equals(key)) {
+                entry.value = value;
+                return;
+            }
+        }
+
+        bucket.add(new Entry<>(key, value));
+        size++;
+    }
+
+    public V get(K key) {
+        int index = getIndex(key);
+        MyLinkedList<Entry<K, V>> bucket = buckets[index];
+
+        for (Entry<K, V> entry : bucket) {
+            if (entry.key.equals(key)) {
+                return entry.value;
+            }
+        }
+        return null;
+    }
+
+    public boolean remove(K key) {
+        int index = getIndex(key);
+        MyLinkedList<Entry<K, V>> bucket = buckets[index];
+
+        for (Entry<K, V> entry : bucket) {
+            if (entry.key.equals(key)) {
+                bucket.remove(entry);
+                size--;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private int getIndex(K key) {
+        if (key == null) {
+            return 0;
+        }
+        return Math.abs(key.hashCode() % buckets.length);
+    }
+
+    private void resize() {
+        int newCapacity = buckets.length * 2;
+        MyLinkedList<Entry<K, V>>[] newBuckets = (MyLinkedList<Entry<K, V>>[]) new MyLinkedList[newCapacity];
+        for (int i = 0; i < newCapacity; i++) {
+            newBuckets[i] = new MyLinkedList<>();
+        }
+
+        for (MyLinkedList<Entry<K, V>> bucket : buckets) {
+            for (Entry<K, V> entry : bucket) {
+                int newIndex = Math.abs(entry.key.hashCode() % newCapacity);
+                newBuckets[newIndex].add(entry);
+            }
+        }
+
+        buckets = newBuckets;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+}
---
+++ src/MyLinkedList.java
@@ -0,0 +1,81 @@
+import java.util.Iterator;
+
+public class MyLinkedList<T> implements Iterable<T> {
+    private Node<T> head;
+    private int size;
+
+    private static class Node<T> {
+        T data;
+        Node<T> next;
+
+        Node(T data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public MyLinkedList() {
+        head = null;
+        size = 0;
+    }
+
+    public void add(T data) {
+        Node<T> newNode = new Node<>(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node<T> current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        size++;
+    }
+
+    public boolean remove(T data) {
+        if (head == null) {
+            return false;
+        }
+
+        if (head.data.equals(data)) {
+            head = head.next;
+            size--;
+            return true;
+        }
+
+        Node<T> current = head;
+        while (current.next != null) {
+            if (current.next.data.equals(data)) {
+                current.next = current.next.next;
+                size--;
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            private Node<T> current = head;
+
+            @Override
+            public boolean hasNext() {
+                return current != null;
+            }
+
+            @Override
+            public T next() {
+                T data = current.data;
+                current = current.next;
+                return data;
+            }
+        };
+    }
+}
